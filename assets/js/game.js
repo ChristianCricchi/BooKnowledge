d49624ec6bfc5c6f264cc255354d5d39f23a15a8
Christian Cricchi <studychristiancricchi@gmail.com>
2024-03-29 20:31:58 +0000

    Added feedback based on correct and incorrect answers

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,21 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-console.log(choices);
+
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [
     {
-        question: 'Inside which HTML element do we put the JavaScript?',
-        choice1: '<script>',
-        choice2: '<javascript>',
-        choice2: '<js>',
-        choice4: '<scripting>',
-        answer: 1,
+        question: "Inside which HTML element do we put the JavaScript?",
+        choice1: "<script>",
+        choice2: "<javascript>",
+        choice2: "<js>",
+        choice4: "<scripting>",
+        answer: 1
     },
     {
         question: "What is the correct syntax for referring to an external script called 'xxx.js'? ",
@@ -23,7 +23,7 @@ let questions = [
         choice2: "<script name='xxx.js'>",
         choice2: "<script src='xxx.js'>",
         choice4: "<script file='xxx.js'>",
-        answer: 3,
+        answer: 3
     },
     {
         question: "How do you write 'Hello World' in an alert box?",
@@ -31,8 +31,8 @@ let questions = [
         choice2: "alertBox('Hello World');",
         choice2: "msg('Hello World');",
         choice4: "alert('Hello World');",
-        answer: 4,
-    },
+        answer: 4
+    }
 ];
 
 // Constants
@@ -50,30 +50,39 @@ startGame = () => {
 GetNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         //go to end page
-        return window.location.assign('/end.html');
+        return window.location.assign("/end.html");
     }
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion;
+    question.innerText = currentQuestion.question;
 
-    choices.forEach((choice) => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
     });
 
     availableQuestions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
 
-choice.forEach((choice) => {
-    choice.addEventListener('click', (e) => {
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
         if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
-        GetNewQuestion();
+        const selectedAnswer = selectedChoice.dataset["number"];
+        
+        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+        selectedChoice.parentElement.classList.remove(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            GetNewQuestion();
+        }, 1000);
+        
     });
 });
 
