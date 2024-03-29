e35876a185ce256cf87700c87101b2b43fee8bc6
Christian Cricchi <studychristiancricchi@gmail.com>
2024-03-29 21:12:33 +0000

    Added question and score counter

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 
 let currentQuestion = {};
@@ -35,7 +37,7 @@ let questions = [
     }
 ];
 
-// Constants
+// CONSTANTS
 
 const CORRECT_BONUS = 1;
 const MAX_QUESTIONS = 3;
@@ -53,6 +55,8 @@ GetNewQuestion = () => {
         return window.location.assign("/end.html");
     }
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/{MAX_QUESTIONS}`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -76,7 +80,11 @@ choices.forEach(choice => {
         
         const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-        selectedChoice.parentElement.classList.remove(classToApply);
+        if (classToApply === "correct") {
+            incrementScore(CORRECT_BONUS);
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
@@ -85,5 +93,10 @@ choices.forEach(choice => {
         
     });
 });
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+};
 
 startGame();
