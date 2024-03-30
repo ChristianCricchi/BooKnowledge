ee42aabd042af52919bace2ce63da60c92158a4f
Christian Cricchi <studychristiancricchi@gmail.com>
2024-03-30 10:45:58 +0000

    Added to end.js: save high score in local storage.

--- assets/js/end.js
+++ assets/js/end.js
@@ -2,7 +2,10 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentItem');
-finalScore.innerText = mosteRecentScore;
+const highScore = JSON.parse(localStorage.getItem('highScore'))  || [];
+const MAX_HIGH_SCORE = 10;
+
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.dasabled = ! username.ariaValueMax;
@@ -11,3 +14,15 @@ username.addEventListener('keyup', () => {
 saveHighScore = (e) => {
     e.preventDefault();
 }
+
+const score = {
+    score: mostRecentScore,
+    name: username.ariaValueMax,
+};
+
+highScore.push(score);
+highScore.sort((a, b) => b.score - a.score);
+highScore.splice(5);
+
+localStorage.setItem('highScore', JSON.stringify(highScore));
+window.location.assign('/');
