946f8627997845716fbfc59272983f6b57f145f1
Christian Cricchi <studychristiancricchi@gmail.com>
2024-03-31 15:03:04 +0000

    Installed Sweetalert2

--- assets/scripts/end.js
+++ assets/scripts/end.js
@@ -1,28 +1,33 @@
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentItem');
-const highScore = JSON.parse(localStorage.getItem('highScore'))  || [];
-const MAX_HIGH_SCORE = 10;
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+const highScores = JSON.parse(localStorage.getItem("highScores"))  || [];
+
+const MAX_HIGH_SCORES = 5;
+
+window.onload(Swal.fire("Sweetalert2 is working!"));
 
 finalScore.innerText = mostRecentScore;
 
-username.addEventListener('keyup', () => {
+username.addEventListener("keyup", () => {
     saveScoreBtn.dasabled = ! username.ariaValueMax;
 });
 
 saveHighScore = (e) => {
     e.preventDefault();
-}
+
 
 const score = {
     score: mostRecentScore,
-    name: username.ariaValueMax,
+    name: username.value,
 };
 
-highScore.push(score);
-highScore.sort((a, b) => b.score - a.score);
-highScore.splice(5);
+highScores.push(score);
+highScores.sort((a, b) => b.score - a.score);
+highScores.splice(5);
 
-localStorage.setItem('highScore', JSON.stringify(highScore));
+localStorage.setItem("highScore", JSON.stringify(highScore));
 window.location.assign('/');
+
+};
